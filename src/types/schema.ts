df7818be6b481fc74acc4829e06134f0b7444ce6
Parallax18 <rhythmist204@gmail.com>
2024-08-14 18:15:12 +0100

    feat: basic component setup

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -16,3 +16,70 @@ export interface IComponent {
 }
 
 export type IComponentWithIndex = IComponent & { index?: number };
+
+export type ITag =
+  | "div"
+  | "span"
+  | "p"
+  | "button"
+  | "img"
+  | "a"
+  | "input"
+  | "form"
+  | "section"
+  | "header"
+  | "footer"
+  | "nav"
+  | "ul"
+  | "li"
+  | "h1"
+  | "h2"
+  | "h3"
+  | "h4"
+  | "h5"
+  | "h6";
+
+export interface IElement {
+  name?: string;
+  id?: string;
+  tag: ITag;
+  thumbnail?: string;
+  category?: string;
+  styles?: Record<string, string | number>; // Inline HTML styles
+  styleClassesLight?: string; // Tailwind class names for light mode
+  styleClassesDark?: string; // Tailwind class names for dark mode
+  attributes?: Record<string, any>;
+  innerText?: string;
+  parentId?: string; // Optional: Reference for context, if needed
+  siblingsIds?: string[]; // Optional: Reference for context, if needed
+  level: number; // Optional: For hierarchy or ordering
+  childrenIds?: string[]; // Optional: Reference for context, if needed
+  eventHandlers?: Record<string, Function>;
+  bindings?: Record<string, any>;
+  conditionalRender?: () => boolean;
+}
+
+export interface Component {
+  id: string;
+  tag: ITag;
+  name: string;
+  styleEntityId: string;
+  level: number;
+  childrenIds?: string[];
+  parentId?: string;
+  innerText?: string;
+}
+
+export interface StyleEntity {
+  id: string;
+  name: string;
+  variantName: string;
+  classes: Classes;
+  isDefault: boolean;
+}
+
+export interface Classes {
+  light: string;
+  dark: string;
+  neutral: string;
+}
